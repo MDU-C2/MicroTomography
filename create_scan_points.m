103de4fbf9d027d9c456569f47157530f5f4a229
Victor Aziz <vaz17001@student.mdu.se>
2022-10-21 11:08:14 +0200

    scan protocol ver1

--- create_scan_points.m
+++ create_scan_points.m
@@ -1,28 +1,15 @@
-% This is a scanning protocol as mentioned in the journal 
-% "Surface Estimation for Microwave Imaging"
-% Link to the journal : 
-% https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5539471/
-%=========================================================================%
-
-% The algoritm should know the coordinate of the center of the hole in the 
-% table and The diameter of the whole. 
-
-% The function work in the coordinate system that have the hole center as
-% origo and 
+function scan_points = create_scan_points(epsilon,z_step_size)
+%CREATE_SCAN_POINTS Summary of this function goes here
+%   Detailed explanation goes here
 
 hole_diameter = 200 ; %CM 
-epsilon = 100;
+
 % it assumes a cylindrical boundary box arround the table hole. 
 % The cylindrical boundary center matches the hole in the table and have
 % the same diameter. 
 % The maximum heigh is assumed to be 20 cm 
 boundary_height = 200;
 
-
-
-% Heights for scanning points 
-
-z_step_size = 5; % Height slice step size
 
 num_z_step = (boundary_height)/(z_step_size); % number of steps 
 
@@ -55,18 +42,6 @@ for i = 1:length(azimuth)
 
 end
 
-%%
-epsilon = 100;
-z_step_size = 5;
-points = create_scan_points(epsilon,z_step_size);
 
-%%
-
-figure;
-hold on;
-% Plot points 
-for point_idx = 1:length(scan_points)
-    scatter3(scan_points(point_idx,1),scan_points(point_idx,2),scan_points(point_idx,3))
 end
 
-vector = []
