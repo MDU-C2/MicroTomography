5be9d97f5408376c9014fdebee3ac5360ff87808
ihe21001 <90029958+ihe21001@users.noreply.github.com>
2022-11-04 15:10:57 +0100

    working

--- LaserpointToRealpoint/Main.m
+++ LaserpointToRealpoint/Main.m
@@ -17,3 +17,9 @@ LaserPoints = CollectLaserPoints.collect(Scanpoints,t); %%LaserPoints = [laser p
 surfacePoint = calcSurfacePoint.calcSurfacePoint(LaserPoints); %%surfacePoint = [x y z]
 
 
+for s = 2:size(surfacePoint,3)
+    for r = 1:size(surfacePoint,1)
+        scatter3(surfacePoint(r,1,s),surfacePoint(r,2,s),surfacePoint(r,3,s),'filled');
+        hold on;
+    end
+end
--- LaserpointToRealpoint/laserPointToRealPoint.m
+++ LaserpointToRealpoint/laserPointToRealPoint.m
@@ -7,30 +7,28 @@
 classdef laserPointToRealPoint
    methods
        function surfacePoints = calcSurfacePoint(obj, LaserPoints)
-           surfacePoints=[];
-           while true %NOT TRUE BUT LENGTH OF STRING
-               %Take next elements in array TRANS ROT
-               surfacePoint = calcSurfacePoint.GetSurfacepoint(t, trans, rot);
-               surfacePoints(end+1) = surfacePoint;
+           surfacePoints=zeros( size(LaserPoints,1), 3, size(LaserPoints,3));
+           for s = 2:size(LaserPoints,3)
+                for r = 1:size(LaserPoints,1)
+                    %Take next elements in array TRANS ROT
+                    surfacePoint = findSurfacePosition(obj, LaserPoints(r,1:3,s), LaserPoints(r,4:6,s), LaserPoints(r,7,s));
+                    surfacePoints(r,:,s) = surfacePoint;
+                end
            end
        end
 
-       function position = GetSurfacepoint(obj, t,transIn,rotIn)
-            %packAndSendPose(obj, 0, transIn, rotIn, t);
-        
-            %[pos, rot] = ReceiveAndUnpackPose(obj, 0, t);
-        
-            scatter3(pos(1),pos(2),pos(3),'filled');
-            hold on;
-            laserlength=20;
-            position = findSurfacePosition(obj, rot,pos,laserlength);
-            scatter3(position(1),position(2),position(3));
-            pause(.1);
-            hold on;
-        end
+%        function position = GetSurfacepoint(obj,p)
+%             scatter3(pos(1),pos(2),pos(3),'filled');
+%             hold on;
+%             laserlength=20;
+%             position = findSurfacePosition(obj, rot,pos,laserlength);
+%             scatter3(position(1),position(2),position(3));
+%             pause(.1);
+%             hold on;
+%         end
         
         
-        function position = findSurfacePosition(obj, rot,posRobot,laserlength)
+        function position = findSurfacePosition(obj,posRobot,rot,laserlength)
             d=[0,0,0];
             laserlength=[0;0;laserlength;1];
         
@@ -40,11 +38,11 @@ classdef laserPointToRealPoint
             T = T1*T2*T3;
         
             position = T*laserlength;
-            position=position(1:3);
+            position=position(1:3)';
         
         end
         
-        function T=createTransformationMatrix(obj,R,D)
+        function T=createTransformationMatrix(~,R,D)
             D=D';
         
             T = [R D];
