654196eeacf3b31bb1ec0bf476ef32290fd38661
Victor Aziz <vaz17001@student.mdu.se>
2022-11-17 09:22:18 +0100

    new scanning function

--- create_scan_points.m
+++ create_scan_points.m
@@ -1,45 +1,44 @@
 function points = create_scan_points(boundary_height,hole_diameter,epsilon,z_step_size, azimuth_angle)
-%CREATE_SCAN_POINTS Summary of this function goes here
-%   Detailed explanation goes here
-
-
-% it assumes a cylindrical boundary box arround the table hole. 
-% The cylindrical boundary center matches the hole in the table and have
-% the same diameter. 
-% The maximum heigh is assumed to be 20 cm 
-
-
-
-num_z_step = (boundary_height)/(z_step_size); % number of steps 
-
-% Z of all scanning heights
+% Create yumi cylindrical scanning positions.
+%=========================================================================%
+% Output 
+% Points 6 Dimensional array  
+    % Positon ( X, Y, Z) in the base coordinate. 
+    % Orientation (eul_Z, eul_Y, eul_X) in the base coordinate orientation.
+%=========================================================================%
+% Base coordinate system 
+    % Base coordinate is in the center of the hole with Z pointing up. 
+    % X points away from the hole center. 
+%=========================================================================%
+% Input
+    % boundary_height: Max height of the scanning cylinder. 
+    % Hole_diameter: Diameter of the hole. 
+    % % epsilon: Extra margin from the hole diamter. 
+    % z_step_size: Step size between scanning points in Z direction. 
+    % azimuth_angle:(degrees) The difference between scanning angles, given
+        % in degrees
+% number of steps in Z direction 
+num_z_step = (boundary_height)/(z_step_size); 
+% Create all Z coordinates for all Z 
 Z = linspace(0,-boundary_height, num_z_step+1);
-
-% Make it better later
+% Create the azimuth angle of all coordinates. 
 azimuth = (pi.*linspace(0,360-azimuth_angle,360/azimuth_angle))./180 ;
-
+% Radius of the hole + an extra margin
 R_epsi = hole_diameter/2 + epsilon;
-
+% Points declaration 
 points = zeros(length(Z),6,length(azimuth));
 
-
 for i = 1:length(azimuth)
- 
     for j = 1:length(Z)
-        % Create coordinate in base space
+        % Create (X, Y, Z) coordinate in base space
         point_c_space(:,1) = R_epsi.*cos(azimuth(i));
         point_c_space(:,2) = R_epsi.*sin(azimuth(i));
         point_c_space(:,3) = Z(j);
-
         % Create Homogeneous Transformations with orientation
         HT = se3(roty(90)*rotx(rad2deg(-azimuth(i))),point_c_space);
-
-        points(j,:,i) = [HT.trvec tform2eul(HT.tform)];
-        
+        % Create points by extracting euler 
+        points(j,:,i) = [HT.trvec tform2eul(HT.tform)];        
     end
-
 end
-
-
 end
 
