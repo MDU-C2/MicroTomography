85384a18576407fb86e08f9b488790873bd1460e
Victor Aziz <vaz17001@student.mdu.se>
2022-11-17 14:32:42 +0100

    real data testing

--- test_realdata.m
+++ test_realdata.m
@@ -2,8 +2,77 @@ clc
 clear all 
 close all 
 
-load surfacePoint11161212.mat ;
+load surfacePoint.mat ;
+% Plot raw data
+figure; hold on
+for spline_ind = 1:size(surfacePoint,3)
+    scatter3(surfacePoint(:,1,spline_ind),surfacePoint(:,2,spline_ind),surfacePoint(:,3,spline_ind))
+    view(3)
+    drawnow
+    
+end
+%%
+% Find minimum 
+min_z = min(surfacePoint(:,3,:),[],'all'); 
+% find index 
+[row_ind, spline_ind] = find(surfacePoint(:,3,:) == min_z);
+% Find min Z point 
+min_point = surfacePoint(row_ind, : , spline_ind);
 
+% add min point to all splines
+for spline_ind = 1:size(surfacePoint,3)
+    first_nan = find(isnan (surfacePoint(:,1,spline_ind)),1, 'first');
+    surfacePoint(first_nan,:,spline_ind) = min_point;
+end
+
+%% Spline interpolation 
+% number of equidistant points to use while interpolating. 
+n_int_points = 8;
+
+% step size for sampling from splines 
+delta_z = 0.5; 
+% Sampling in Z dimension 
+num_points = round(abs(min_z/delta_z));
+z_uniform = linspace(0,min_z,num_points);
+
+points_uniform = zeros(num_points, 3, size(surfacePoint,3));
+
+for spline_ind = 1:size(surfacePoint,3)
+    % Choose interpolation points 
+    first_nan = find(isnan (surfacePoint(:,1,spline_ind)),1, 'first');
+    index_list = round(linspace(1,first_nan-1, n_int_points));
+
+    % Interpolate 
+    points_z = surfacePoint(index_list,3,spline_ind);
+    points_x = surfacePoint(index_list,1,spline_ind);
+    points_y = surfacePoint(index_list,2,spline_ind);
+    ppx = spline (points_z,points_x);
+    ppy= spline (points_z,points_y);
+
+
+    % Create Points 
+    points_uniform(:,1,spline_ind) = ppval(ppx,z_uniform );
+    points_uniform(:,2,spline_ind) = ppval(ppy,z_uniform );
+    points_uniform(:,3,spline_ind) = z_uniform;
+
+end
+%% Plot results
+figure; hold on; 
+for i = 1: size(points_uniform, 3)
+    plot3(points_uniform(:,1,i),points_uniform(:,2,i),points_uniform(:,3,i),'blue')
+    scatter3(points_uniform(:,1,i),points_uniform(:,2,i),points_uniform(:,3,i),'red','*')
+    scatter3(surfacePoint(:,1,i),surfacePoint(:,2,i),surfacePoint(:,3,i),'black', 'filled')    
+end
+legend( 'Splines interpolated', 'Unifrom sample', 'Sampled points')
+
+%%
+figure; hold on
+for spline_ind = 1:size(surfacePoint,3)
+    scatter3(surfacePoint(:,1,spline_ind),surfacePoint(:,2,spline_ind),surfacePoint(:,3,spline_ind))
+    view(3)
+    drawnow
+    
+end
 
 %% Clean files from nan Run once to get files in current folder 
 % for i = 1:size(surfacePoint,3)
@@ -32,8 +101,8 @@ for i = 1:size(surfacePoint,3)
     B = isnan(data);
     data(B(:,1),:)= [];
     scatter3(data(:,1),data(:,2),data(:,3))
-    ppx = spline (data(:,3),data(:,1));
-    ppy= spline (data(:,3),data(:,2));
+    ppx = spline_ind (data(:,3),data(:,1));
+    ppy= spline_ind (data(:,3),data(:,2));
 
     ppoints(:,1,i) = ppval(ppx,zz);
     ppoints(:,2,i) = ppval(ppy,zz);
