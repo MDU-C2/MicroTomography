68b39bf4be6b7abb3a97cad8716a358bd886140f
Victor Aziz <vaz17001@student.mdu.se>
2022-11-18 06:47:36 +0100

    clean the file

--- test_realdata.m
+++ test_realdata.m
@@ -2,7 +2,7 @@ clc
 clear all 
 close all 
 
-load surfacePoint11171430.mat ;
+load surfacePoint11171457.mat ;
 % Plot raw data
 figure; hold on
 for spline_ind = 1:size(surfacePoint,3)
@@ -20,10 +20,10 @@ min_z = min(surfacePoint(:,3,:),[],'all');
 min_point = surfacePoint(row_ind, : , spline_ind);
 
 % add min point to all splines
-for spline_ind = 1:size(surfacePoint,3)
-    first_nan = find(isnan (surfacePoint(:,1,spline_ind)),1, 'first');
-    surfacePoint(first_nan,:,spline_ind) = min_point;
-end
+% for spline_ind = 1:size(surfacePoint,3)
+%     first_nan = find(isnan (surfacePoint(:,1,spline_ind)),1, 'first');
+%     surfacePoint(first_nan,:,spline_ind) = min_point;
+% end
 
 %% Spline interpolation 
 % number of equidistant points to use while interpolating. 
@@ -73,59 +73,8 @@ for spline_ind = 1:size(surfacePoint,3)
     drawnow
     
 end
-
-%% Clean files from nan Run once to get files in current folder 
-% for i = 1:size(surfacePoint,3)
-%     FileName=[num2str(i),'points.csv'];
-%     %csvwrite(FileName,surfacePoint(:,:,i))
-%     data = readtable(FileName);
-%     data = table2array(data);
-%     B = isnan(data);
-%     data(B(:,1),:)= [];
-%     csvwrite(FileName,surfacePoint(:,:,i))  
-% end
 %%
-boundary_height = 100;
-z_step_size = 0.5;
-num_z_step = (boundary_height)/(z_step_size); % number of steps
-zz =  linspace(-z_step_size,-boundary_height, num_z_step);
-
-ppoints = zeros(length(zz),3,21);
-
-figure;hold on;
-for i = 1:size(surfacePoint,3)
-    FileName=[num2str(i),'points.csv'];
-    %csvwrite(FileName,surfacePoint(:,:,i))
-    data = readtable(FileName);
-    data = table2array(data);
-    B = isnan(data);
-    data(B(:,1),:)= [];
-    scatter3(data(:,1),data(:,2),data(:,3))
-    ppx = spline_ind (data(:,3),data(:,1));
-    ppy= spline_ind (data(:,3),data(:,2));
-
-    ppoints(:,1,i) = ppval(ppx,zz);
-    ppoints(:,2,i) = ppval(ppy,zz);
-    ppoints(:,3,i) = zz;
-
-end
 
 
-
-%%
-    figure;hold on;
-for i = 1:size(ppoints,3)
-    
-    scatter3(ppoints(:,1,i),ppoints(:,2,i),ppoints(:,3,i))
-    
-end
-
-%%
-figure;hold on;
-for i = 1:size(surfacePoint,3)
-    scatter3(points(:,1,i),points(:,2,i),points(:,3,i))
-    view(3)
-    drawnow
-end
 
 
