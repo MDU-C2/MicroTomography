87916a6f4615c9bc55a372ce7bc4f00399237d2b
Victor Aziz <vaz17001@student.mdu.se>
2022-11-18 14:47:16 +0100

    modified create scan points

--- Scanninng Protocol/create_scan_points.m
+++ Scanninng Protocol/create_scan_points.m
@@ -1,4 +1,4 @@
-function points = create_scan_points(boundary_height,hole_diameter,epsilon,z_step_size, azimuth_angle)
+function points = create_scan_points(boundary_height,hole_diameter,epsilon,z_step_size, azimuth_angle, laser_angle, plot)
 % Create yumi cylindrical scanning positions.
 %=========================================================================%
 % Output 
@@ -16,7 +16,8 @@ function points = create_scan_points(boundary_height,hole_diameter,epsilon,z_ste
     % % epsilon: Extra margin from the hole diamter. 
     % z_step_size: Step size between scanning points in Z direction. 
     % azimuth_angle:(degrees) The difference between scanning angles, given
-        % in degrees
+    % in degrees
+    % Laser angle: The angle of the laser with Z axis in degree
 % number of steps in Z direction 
 num_z_step = (boundary_height)/(z_step_size); 
 % Create all Z coordinates for all Z 
@@ -28,6 +29,12 @@ R_epsi = hole_diameter/2 + epsilon;
 % Points declaration 
 points = zeros(length(Z),6,length(azimuth));
 
+if plot  == true
+    fig = figure;
+    axis = axes;
+    hold(axis, "on")
+end
+
 for i = 1:length(azimuth)
     for j = 1:length(Z)
         % Create (X, Y, Z) coordinate in base space
@@ -35,7 +42,11 @@ for i = 1:length(azimuth)
         point_c_space(:,2) = R_epsi.*sin(azimuth(i));
         point_c_space(:,3) = Z(j);
         % Create Homogeneous Transformations with orientation
-        HT = se3(roty(90)*rotx(rad2deg(-azimuth(i))),point_c_space);
+        HT = se3(roty(90+laser_angle)*rotx(rad2deg(-azimuth(i))),point_c_space);
+
+        if plot == true 
+            show_HT(fig,axis, HT,20 )
+        end
         % Create points by extracting euler 
         points(j,:,i) = [HT.trvec tform2eul(HT.tform)];        
     end
