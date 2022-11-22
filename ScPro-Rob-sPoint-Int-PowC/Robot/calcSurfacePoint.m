b8a57c0d88ba979f3f5d491f5412da04c2e8915b
ihe21001 <90029958+ihe21001@users.noreply.github.com>
2022-11-22 10:59:26 +0100

    Fix in calc surf points

--- ScPro-Rob-sPoint-Int-PowC/Robot/calcSurfacePoint.m
+++ ScPro-Rob-sPoint-Int-PowC/Robot/calcSurfacePoint.m
@@ -11,6 +11,18 @@ function surfacePoints = calcSurfacePoint(LaserPoints)
                 surfacePoints(r,:,s) = [NaN,NaN,NaN];
             end
 
+            scatter3(LaserPoints(r,1,s),LaserPoints(r,2,s),LaserPoints(r,3,s),'filled')
+            hold on;
+            scatter3(surfacePoints(r,1,s),surfacePoints(r,2,s),surfacePoints(r,3,s));
+            hold on;
         end
    end
 end
+
+%             scatter3(pos(1),pos(2),pos(3),'filled');
+%             hold on;
+%             laserlength=20;
+%             position = findSurfacePosition(obj, rot,pos,laserlength);
+%             scatter3(position(1),position(2),position(3));
+%             pause(.1);
+%             hold on;
--- ScPro-Rob-sPoint-Int-PowC/Robot/findSurfacePosition.m
+++ ScPro-Rob-sPoint-Int-PowC/Robot/findSurfacePosition.m
@@ -2,7 +2,7 @@ function position = findSurfacePosition(posRobot,rot,laserlength)
     d=[0,0,0];
     laserlength=[0;0;laserlength;1];
 
-    T1=createTransformationMatrix(rotx(rot(3)),posRobot);
+    T1=createTransformationMatrix(rotz(rot(3)),posRobot);
     T2=createTransformationMatrix(roty(rot(2)),d);
     T3=createTransformationMatrix(rotx(rot(1)),d);
     T = T1*T2*T3;
