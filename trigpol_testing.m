eecbb960d4e7dc10ac914e893daa4798b7fb8f48
Victor Aziz <vaz17001@student.mdu.se>
2022-11-22 14:16:36 +0100

    COMPARING

--- trigpol_testing.m
+++ trigpol_testing.m
@@ -2,11 +2,11 @@ clear all
 close all
 clc
 
-load surfacePoint11221058.mat
+load surfacePoint11161346.mat
 
 
 figure; hold on;
-for i = 1: size(surfacePoints,3)
+for i = 1%: size(surfacePoints,3)
 
    plot(surfacePoints(:,3,i),surfacePoints(:,1,i), 'Marker','*')
 end
@@ -28,13 +28,34 @@ for i = 1: size(surfacePoints,1)
    plot(squeeze(surfacePoints(i,2,:)),squeeze(surfacePoints(i,1,:)), 'Marker','*')
 end
 %%
+clear all 
+close all
+clc
+
+load surfacePoint11161346.mat
+
+old = load ('angle10_surfacePoint11221058.mat');
+old = old.surfacePoints;
+stl_file = stlread('symmetricalBreastModel.STL');
+real_points = rotx(90)*stl_file.Points';
+real_points = real_points';
+
+real_points(:,1) = real_points(:,1) -60;
+real_points(:,2) = real_points(:,2) +60;
+real_points(:,3) = real_points(:,3) -120;
+
 figure; hold on;
 for i = 1:size(surfacePoints,3)
-    scatter3(surfacePoints(:,1,i),surfacePoints(:,2,i),surfacePoints(:,3,i) )
+    scatter3(surfacePoints(:,1,i),surfacePoints(:,2,i),surfacePoints(:,3,i), 'filled', 'black' )
+    scatter3(old(:,1,i),old(:,2,i),old(:,3,i), 'filled', 'blue' )
     view(3)
-    pause(1)
-end
 
+end
+hold on; 
+scatter3(real_points(:,1),real_points(:,2),real_points(:,3),'Marker','.')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
 %%
 spline_data = surfacePoints(:,:,3);
 
