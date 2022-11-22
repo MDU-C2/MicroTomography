b9f737999eeff7cc4a56f7a2c83d0d61ca31f662
Victor Aziz <vaz17001@student.mdu.se>
2022-11-22 10:59:54 +0100

    try laser data

--- trigpol_testing.m
+++ trigpol_testing.m
@@ -23,3 +23,29 @@ for i = 1:size(surfacePoint,3)
     view(3)
     pause(1)
 end
+
+%%
+clear all 
+close all
+clc
+
+load LaserPoint11221018.mat
+ppoints = zeros(size(LaserPoints,1),3,size(LaserPoints,3))
+for i = 1:size(LaserPoints,3)
+    for j = 1:size(LaserPoints,1)
+        pos = LaserPoints(j,1:3,i);
+        euler = LaserPoints(j,4:6,i);
+        rotm = eul2rotm(deg2rad(euler));
+        HT = se3(rotm,pos);
+        point_laser_base = [0,0,LaserPoints(j,7,i)];
+        ppoints(j,:,i) = HT.transform(point_laser_base);
+
+    end
+    
+end
+figure; hold on;
+for i = 1:size(ppoints,3)
+    scatter3(ppoints(:,1,i),ppoints(:,2,i),ppoints(:,3,i) )
+    view(3)
+    pause(1)
+end
