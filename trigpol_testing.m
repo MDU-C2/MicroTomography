aa78991665f7c1b8c9bac5f2f2384b6598d9662a
Victor Aziz <vaz17001@student.mdu.se>
2022-11-22 10:33:09 +0100

    added new angle data

--- trigpol_testing.m
+++ trigpol_testing.m
@@ -2,18 +2,24 @@ clear all
 close all
 clc
 
-load surfacePoint11211333.mat
+load surfacePoint11221018.mat
 
 
 figure; hold on;
 data = zeros(size(surfacePoint,1),size(surfacePoint,2),size(surfacePoint,3));
 
 outliner = zeros(size(surfacePoint,1),size(surfacePoint,2),size(surfacePoint,3))
-for i = 1: size(surfacePoint,3)
-    
-    first_nan = find(isnan (surfacePoint(5:end,:,i)),1, 'first');% start from value 5 as first value may be nan sometimes 
-    surfacePoint(first_nan:end, : , i) = nan; 
-    outliner(:,:,i) = isoutlier(surfacePoint(:,:,i), "movmedian",2);
-
-    plot(surfacePoint(:,3,i),surfacePoint(:,2,i), 'Marker','*')
+% for i = 1: size(surfacePoint,3)
+%     
+% %     first_nan = find(isnan (surfacePoint(5:end,:,i)),1, 'first');% start from value 5 as first value may be nan sometimes 
+% %     surfacePoint(first_nan:end, : , i) = nan; 
+% %     outliner(:,:,i) = isoutlier(surfacePoint(:,:,i), "movmedian",2);
+% 
+%  %   plot(surfacePoint(:,3,i),surfacePoint(:,1,i), 'Marker','*')
+% end
+figure; hold on;
+for i = 1:size(surfacePoint,3)
+    scatter3(surfacePoint(:,1,i),surfacePoint(:,2,i),surfacePoint(:,3,i) )
+    view(3)
+    pause(1)
 end
