64d5c62ff537d4e053794727ccb716ec39a0b1b1
Victor Aziz <vaz17001@student.mdu.se>
2022-11-22 13:46:47 +0100

    added data and plots

--- trigpol_testing.m
+++ trigpol_testing.m
@@ -2,50 +2,42 @@ clear all
 close all
 clc
 
-load surfacePoint11221018.mat
+load surfacePoint11221058.mat
 
 
 figure; hold on;
-data = zeros(size(surfacePoint,1),size(surfacePoint,2),size(surfacePoint,3));
+for i = 1: size(surfacePoints,3)
 
-outliner = zeros(size(surfacePoint,1),size(surfacePoint,2),size(surfacePoint,3))
-% for i = 1: size(surfacePoint,3)
-%     
-% %     first_nan = find(isnan (surfacePoint(5:end,:,i)),1, 'first');% start from value 5 as first value may be nan sometimes 
-% %     surfacePoint(first_nan:end, : , i) = nan; 
-% %     outliner(:,:,i) = isoutlier(surfacePoint(:,:,i), "movmedian",2);
-% 
-%  %   plot(surfacePoint(:,3,i),surfacePoint(:,1,i), 'Marker','*')
-% end
+   plot(surfacePoints(:,3,i),surfacePoints(:,1,i), 'Marker','*')
+end
+%%
 figure; hold on;
-for i = 1:size(surfacePoint,3)
-    scatter3(surfacePoint(:,1,i),surfacePoint(:,2,i),surfacePoint(:,3,i) )
+for i = 1: size(surfacePoints,3)
+
+   plot(surfacePoints(:,3,i),surfacePoints(:,2,i), 'Marker','*')
+end
+%%
+figure; hold on;
+for i = 1: size(surfacePoints,3)
+   plot(surfacePoints(:,1,i),surfacePoints(:,2,i), 'Marker','*')
+end
+%% 
+ 
+for i = 1: size(surfacePoints,1)
+   figure;
+   plot(squeeze(surfacePoints(i,2,:)),squeeze(surfacePoints(i,1,:)), 'Marker','*')
+end
+%%
+figure; hold on;
+for i = 1:size(surfacePoints,3)
+    scatter3(surfacePoints(:,1,i),surfacePoints(:,2,i),surfacePoints(:,3,i) )
     view(3)
     pause(1)
 end
 
 %%
-clear all 
-close all
-clc
+spline_data = surfacePoints(:,:,3);
 
-load LaserPoint11221018.mat
-ppoints = zeros(size(LaserPoints,1),3,size(LaserPoints,3))
-for i = 1:size(LaserPoints,3)
-    for j = 1:size(LaserPoints,1)
-        pos = LaserPoints(j,1:3,i);
-        euler = LaserPoints(j,4:6,i);
-        rotm = eul2rotm(deg2rad(euler));
-        HT = se3(rotm,pos);
-        point_laser_base = [0,0,LaserPoints(j,7,i)];
-        ppoints(j,:,i) = HT.transform(point_laser_base);
+figure;
+plot(spline_data(:,3),spline_data(:,2),'Marker','*')
 
-    end
-    
-end
-figure; hold on;
-for i = 1:size(ppoints,3)
-    scatter3(ppoints(:,1,i),ppoints(:,2,i),ppoints(:,3,i) )
-    view(3)
-    pause(1)
-end
