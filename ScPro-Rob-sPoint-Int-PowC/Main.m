fff8f71254c8737be9007e6f58f4d02d890c1386
ihe21001 <90029958+ihe21001@users.noreply.github.com>
2022-11-23 16:18:48 +0100

    Moving files to map

--- ScPro-Rob-sPoint-Int-PowC/Main.m
+++ ScPro-Rob-sPoint-Int-PowC/Main.m
@@ -14,12 +14,12 @@ Mesh = scanBreast(t);
 function Mesh = scanBreast(t)
     %Scanpoints = create_scan_points(150,150,50,5); %% Scanpoints = []
     Scanpoints = create_scan_points(150,150,50,5,18,0,false);
-
+    
     %Test different points randomly from scanpoints
     %TestRandPoints(Scanpoints(:,:,:),t);
     
     tic
-
+    
     %%Collect surface points:
     LaserPoints = collectLaserPoint(Scanpoints(:,:,:),t); %%LaserPoints = [laser robPos robRot]
     surfacePoint = calcSurfacePoint(LaserPoints); %%surfacePoint = [x y z]
@@ -32,9 +32,9 @@ function Mesh = scanBreast(t)
     poToPowerCrust = permute(ppoints,[1 3 2]);
     poToPowerCrust = reshape(poToPowerCrust,[],size(ppoints,2),1);
     [~,Mesh,~,~] = PowerCrust(poToPowerCrust,0);
-
+    
     toc
-
+    
     figure
     shp = alphaShape(Mesh(:,1),Mesh(:,2),Mesh(:,3));
     shape.alpha = 200;
