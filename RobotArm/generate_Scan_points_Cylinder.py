Fixed scanning pattern for cylindar rotation

RobotArm/generate_Scan_points_Cylinder.py
@@ -2,13 +2,11 @@ import numpy as np
 
 from pytransform3d.rotations import (
     matrix_from_axis_angle,
-    quaternion_from_axis_angle,
+    quaternion_from_matrix,
 )
 
 def generate_scan_points_cylinder(diameter, zStepSize, zMax):
     R1 = matrix_from_axis_angle(np.array([0, 1, 0, np.pi / 2]))
-
-    diameter = 10
     azimuth = (np.pi * np.linspace(0, 360, 20)) / 180
     points = []
     quaternions = []
@@ -18,10 +16,10 @@ def generate_scan_points_cylinder(diameter, zStepSize, zMax):
         for h in z:
             x = diameter * np.cos(angle)
             y = diameter * np.sin(angle)
-            R2 = matrix_from_axis_angle(np.array([1.0, 0.0, 0.0, np.pi - angle]))
+            R2 = matrix_from_axis_angle(np.array([1, 0, 0, np.pi - angle]))
             R = np.matmul(R1, R2)
 
-            points.append([[x, y, h], quaternion_from_axis_angle(np.array([0.0, 0.0, 1.0, np.pi + angle]))])
+            points.append([[x, y, h], quaternion_from_matrix(R)])
 
     
     return points