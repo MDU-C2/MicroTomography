d75428bdfba2df7908e5f44521efe4440d00fc5a
Joel <te11c.joel.josefsson@gmail.com>
2023-10-06 14:07:14 +0200

    Cleanup of out commented code

--- RobotArm/generate_Scan_points_Cylinder.py
+++ RobotArm/generate_Scan_points_Cylinder.py
@@ -1,14 +1,18 @@
 import numpy as np
 
-from pytransform3d.rotations import *
-
-"""matrix_from_axis_angle,
-    quaternion_from_matrix,
-    matrix_from_quaternion,"""
+from pytransform3d.rotations import (
+    matrix_from_quaternion,
+    concatenate_quaternions,
+    quaternion_from_axis_angle,
+)
 
 
 def generate_scan_points_cylinder(
-    radius: (int | float), zStepSize: int, zMin: int, azimuthPoints: int, zOffset=0
+    radius: (int | float),
+    zStepSize: int,
+    zMin: int,
+    azimuthPoints: int,
+    zOffset: (int | float) = 0,
 ):
     """Generates points in a cylindrical pattern with quaternion angles pointing inwards toward (0, 0) in each z plane
 
@@ -22,6 +26,8 @@ def generate_scan_points_cylinder(
         The lowest point of the cylinder
     azimuthPoints : int
         Number of points in the azimuth angle
+    zOffset: int or float
+        Sets an offset in the z-axis
 
     Returns
     -------
@@ -30,7 +36,6 @@ def generate_scan_points_cylinder(
     """
     if zMin > 0:
         raise AssertionError("zMin must be negative")
-    # R1 = matrix_from_axis_angle(np.array([0, 1, 0, np.pi / 2]))
     q1 = quaternion_from_axis_angle(np.array([0, 1, 0, np.pi / 2]))
     azimuth = (np.pi * np.linspace(0, 360 - (360 / azimuthPoints), azimuthPoints)) / 180
     points = []
@@ -41,21 +46,22 @@ def generate_scan_points_cylinder(
         for h in z:
             x = radius * np.cos(angle)
             y = radius * np.sin(angle)
-            # R2 = matrix_from_axis_angle(np.array([1, 0, 0, np.pi - angle]))
             q2 = quaternion_from_axis_angle(np.array([1, 0, 0, np.pi - angle]))
-            # R = np.matmul(R1, R2)
             q = concatenate_quaternions(q1=q1, q2=q2)
 
-            # points.append([np.array([x, y, h]), quaternion_from_matrix(R)])
             points.append([np.array([x, y, h]), q])
 
     return points
 
 
 def generate_scan_points_halfSphere(
-    radius: (int | float), azimuthPoints: int, elevationPoints: int, zMin=0
+    radius: (int | float),
+    azimuthPoints: int,
+    elevationPoints: int,
+    zMin=0,
+    zOffset: (int | float) = 0,
 ):
-    """Generates points in a half-sphere below z=0 and the quaternion angles such that the z-axis always points to (0, 0, 0)
+    """Generates points in a half-sphere below z=0 and the quaternion angles such that the z-axis of the end effector always points to (0, 0, 0)
 
     Parameters
     ----------
@@ -67,6 +73,8 @@ def generate_scan_points_halfSphere(
         Number of points in the elevation plane
     zMin : optional
         Sets how low the half-sphere should go in the z-axis
+    zOffset: int or float
+        Sets an offset in the z-axis
 
     Returns
     -------
@@ -86,16 +94,12 @@ def generate_scan_points_halfSphere(
         for phi in elevation:
             x = radius * np.sin(phi) * np.cos(theta)
             y = radius * np.sin(phi) * np.sin(theta)
-            z = radius * np.cos(phi) * (zParam)
+            z = (radius * np.cos(phi) * (zParam)) + zOffset
 
-            # R2 = matrix_from_axis_angle(np.array([0, 1, 0, np.pi + phi]))
             q1 = quaternion_from_axis_angle(np.array([0, 1, 0, np.pi + phi]))
-            # R1 = matrix_from_axis_angle(np.array([0, 0, 1, theta]))
             q2 = quaternion_from_axis_angle(np.array([0, 0, 1, theta]))
             q = concatenate_quaternions(q1=q2, q2=q1)
-            # R = np.matmul(R1, R2)
-            # points.append([np.array([x, y, z]), quaternion_from_matrix(R)])
-            plot_basis(R=matrix_from_quaternion(q), p=np.array([x, y, z]))
+
             points.append([np.array([x, y, z]), q])
 
     return points
@@ -123,28 +127,3 @@ def transformLaserDistance(point: list, laserDistance: (int | float)):
     p_new = (R @ p) + laserCoordinates
 
     return p_new
-
-
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-
-# points = generate_scan_points_halfSphere(10, 10, 10, -20)
-points = generate_scan_points_cylinder(100, 10, -100, 10, -10)
-ax.scatter(
-    [coord[0][0] for coord in points],
-    [coord[0][1] for coord in points],
-    [coord[0][2] for coord in points],
-)
-plt.show()
-
-q1 = quaternion_from_axis_angle(np.array([0, 1, 0, np.pi / 2]))
-q2 = quaternion_from_axis_angle(np.array([1, 0, 0, np.pi]))
-
-R1 = matrix_from_axis_angle(np.array([0, 1, 0, np.pi / 2]))
-R2 = matrix_from_axis_angle(np.array([1, 0, 0, np.pi]))
-R = R1 @ R2
-print(quaternion_from_matrix(R))
-
-print(concatenate_quaternions(q1, q2))
