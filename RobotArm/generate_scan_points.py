88de40c1bcd30df947385f36701c157b4595524b
Joel <te11c.joel.josefsson@gmail.com>
2023-11-08 10:50:32 +0100

    made floats available in z axis

--- RobotArm/generate_scan_points.py
+++ RobotArm/generate_scan_points.py
@@ -62,7 +62,7 @@ def generate_scan_points_cylinder(
     azimuth = np.linspace(0, 2 * np.pi - ((2 * np.pi) / azimuth_points), azimuth_points)
     points = []
 
-    z = [h for h in reversed(range(z_min, z_offset + z_stepsize, z_stepsize))]
+    z = np.arange(z_offset, z_min - 1e-10, -z_stepsize)
 
     for angle in azimuth:
         for h in z:
