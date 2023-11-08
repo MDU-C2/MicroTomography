9d43e1853b88ee4a58c57b907c23354159e0305d
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2023-11-08 12:00:11 +0100

    Update some code

--- ObjectReconstruction/Surface_Reconstruction.py
+++ ObjectReconstruction/Surface_Reconstruction.py
@@ -1,16 +1,18 @@
-##
+## ## Author : Filip Lindhe
+
 # Modules
 ##
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 import scipy as sp
 import numpy as np
-import pyvista as pv
+
+# import pyvista as pv
 import open3d as o3d
 import time
 
 
-class surface_Reconstruction:
+class surface_reconstruction:
     def delaunay_original(points, save):
         t = time.time()
         ############ Works fine but it's not perfect. Requires very good data to provide good results.#############
@@ -25,26 +27,29 @@ class surface_Reconstruction:
 
         mesh = Triangular.delaunay_3d()  # Creates mesh using delaunay 3d triangles
 
+        points = mesh.GetPoints().GetData()
+        triangles = mesh.cell_data_to_point_data()
         # mesh = delaunay_tri
         # Plot the mesh
         elapsed = time.time() - t
         print("Time to do surface reconstruction:", elapsed)
         plotter = pv.Plotter()
         plotter.add_mesh(mesh, show_edges=False, color="white")
-        plotter.add_points(mesh.points, color="red", point_size=5)
+        plotter.add_points(mesh.points, color="red", point_size=10)
         plotter.show()
         if save == True:
             mesh.triangle_normals = o3d.utility.Vector3dVector([])
             o3d.io.write_triangle_mesh(
                 "mesh.obj", mesh, print_progress=True
             )  # Writes the file without any warnings.
+        return mesh
 
-    def alpha_Shape(points, save):
+    def alpha_shape(points, save):
         t = time.time()
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points)
         pcd.estimate_normals()
-        alpha = 20
+        alpha = 50
         # o3d.visualization.draw_geometries([pcd])
         # print(pcd)
         norm_Radius = 100
@@ -72,7 +77,7 @@ class surface_Reconstruction:
         elapsed = time.time() - t
         print("Time to do surface reconstruction:", elapsed)
         # o3d.visualization.draw_geometries([pcd,tetra_mesh], mesh_show_back_face=True)
-        # o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+        o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
         # o3d.visualization.draw_geometries([pcd], point_show_normal=True)
 
         if save == True:
@@ -81,7 +86,7 @@ class surface_Reconstruction:
                 "mesh.obj", mesh, print_progress=True
             )  # Writes the file without any warnings.
 
-    def ball_Pivoting(points, save):
+    def ball_pivoting(points, save):
         ############ Not very good for unstructured data. Need to find good radius of the balls, which is hard?. (Super slow cause looping radius)#############
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points)
@@ -92,12 +97,12 @@ class surface_Reconstruction:
         )
         o3d.visualization.draw_geometries([pcd, rec_mesh], mesh_show_back_face=True)
         if save == True:
-            mesh.triangle_normals = o3d.utility.Vector3dVector([])
+            rec_mesh.triangle_normals = o3d.utility.Vector3dVector([])
             o3d.io.write_triangle_mesh(
-                "mesh.obj", mesh, print_progress=True
+                "mesh.obj", rec_mesh, print_progress=True
             )  # Writes the file without any warnings.
 
-    def poisson_surfRecon(points, save):
+    def poisson_surface_reconstruction(points, save):
         t = time.time()
         ############ Works the best. Need to figure out how to mesh the top of the object where triangles are "too long".#############
         ############ Needs sufficient enough data ###############
@@ -110,10 +115,6 @@ class surface_Reconstruction:
         pcd.estimate_normals(
             search_param=o3d.geometry.KDTreeSearchParamHybrid(norm_Radius, norm_NN)
         )
-        """o3d.geometry.PointCloud.orient_normals_to_align_with_direction( 
-            pcd, 
-            orientation_reference=np.array([0., 0., 1.])
-        )"""
 
         ##Orient normals to point "outward"
 
@@ -121,9 +122,6 @@ class surface_Reconstruction:
         pcd.orient_normals_consistent_tangent_plane(
             orient_Norm_Knn
         )  ##Very expensive time and resource wise.
-        # normals = np.asarray(pcd.normals)
-        # normals = -normals
-        # pcd.normals = o3d.utility.Vector3dVector(normals)
 
         with o3d.utility.VerbosityContextManager(
             o3d.utility.VerbosityLevel.Debug
@@ -136,27 +134,13 @@ class surface_Reconstruction:
         # mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh).fill_holes().to_legacy() Can be used to fill large holes with a mesh. Not really usefull
         mesh.compute_vertex_normals()
         mesh.paint_uniform_color(np.array([[0.5], [0.5], [0.5]]))
-        """o3d.visualization.draw_geometries([mesh],mesh_show_back_face=True)
-        densities = np.asarray(densities)
-        density_colors = plt.get_cmap('plasma')(
-            (densities - densities.min()) / (densities.max() - densities.min()))
-        density_colors = density_colors[:, :3]
-        density_mesh = o3d.geometry.TriangleMesh()
-        density_mesh.vertices = mesh.vertices
-        density_mesh.triangles = mesh.triangles
-        density_mesh.triangle_normals = mesh.triangle_normals
-        density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
-        o3d.visualization.draw_geometries([density_mesh])                                       
-        vertices_to_remove = densities < np.quantile(densities, 0.1) 
-        mesh.remove_vertices_by_mask(vertices_to_remove)"""
-        # V = o3d.geometry.TriangleMesh.get_volume(mesh)
-        # print("Volume of mesh is : " ,V)
+        o3d.visualization.draw_geometries([pcd, rec_mesh], mesh_show_back_face=True)
         elapsed = time.time() - t
         print("Time to do surface reconstruction:", elapsed)
-        # o3d.visualization.draw_geometries([pcd], point_show_normal=True)
-        # o3d.visualization.draw_geometries([mesh],mesh_show_back_face=True)
+
         if save == True:
             mesh.triangle_normals = o3d.utility.Vector3dVector([])
             o3d.io.write_triangle_mesh(
                 "meshNew.obj", mesh, print_progress=True
             )  # Writes the file without any warnings.
+        return mesh
