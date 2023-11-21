changed quaternion to show the correct values

ObjectReconstruction/choose_points_microwave.py
@@ -74,6 +74,7 @@ def ray_cast_points(recon_mesh, choosenPoints, distance_from_mesh):
 
         q = np.append(q, np.sin(theta / 2) * b_hat[2])
         quat.append(q)
+        vector = [0, 0, -1]
         y_vector = np.cross(n, vector)
         x_vector = np.cross(y_vector, n)
 
@@ -89,10 +90,10 @@ def ray_cast_points(recon_mesh, choosenPoints, distance_from_mesh):
         quaternions_test = mat.to_quaternion()
         quaternion.append(quaternions_test)
         """plot_basis(R=matrix_from_quaternion(q))
-
-        plt.show()
         plot_basis(R=matrix_from_quaternion(quaternions_test))
-        plt.show()"""
+        plt.show()
+
+        # plt.show()"""
 
     return closestPoints, quaternion
 

ObjectReconstruction/main.py
@@ -95,7 +95,7 @@ average_error_1 = line_trace(GT_mesh, recon_mesh, test, data)
 
 # ChosenPoints Functions gets the closest points on the mesh
 choosen_points = np.array(
-    [[50, -50, -60]]
+    [[60, 0, -50]]
 )  # Needs atleast two points to be able to plot them.
 distance_from_mesh = 2  # in mm
 closestPoints, quats = ray_cast_points(