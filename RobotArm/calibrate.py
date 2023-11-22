53b75306677ada3d451eec947ae05542da7291c3
Samuel Wågbrant <swagbrant@gmail.com>
2023-11-22 09:24:55 +0100

    tested vector analyzer

--- RobotArm/calibrate.py
+++ RobotArm/calibrate.py
@@ -5,10 +5,9 @@ pathname = os.getcwd()
 
 sys.path.append(r"/home/mwi/Repositories/MicroTomography")
 
-
 from RobotArm.generate_scan_points import generate_scan_points_cylinder
 
-from RobotArm import robot_control
+from RobotArm import robot_control as robot_control
 from time import sleep
 from Laser.optoNCDT1402 import optoNCDT1402
 
@@ -29,68 +28,73 @@ step = 10
 depth = -110
 offset = -110
 azi = 16
-# q1 =
-q1 = [9.99964080e-01, 8.39557165e-03, 1.16335116e-03, 9.30162807e-07]
-WOBJ_POS = [-5.27669, -4.89651, 764.097]
-
 best = 10
 
-points = generate_scan_points_cylinder(radius, step, depth, azi, offset, laser_angle=90)
+#q1 = [9.99964080e-01, 8.39557165e-03, 1.16335116e-03, 9.30162807e-07]
+#WOBJ_POS = [-5.27669, -4.89651, 764.097]
 
-while best > 0.01:
-    robot_control.return_robot_to_start(robot)
-    robot_control.set_reference_coordinate_system(robot, [WOBJ_POS, q1])
+def calibration(WOBJ_POS, quaterion):
 
-    laser_data = []
+    q1 = quaterion
 
-    for point in points:
-        alteredPoint = [point[0], [1, 0, 0, 0]]
+    points = generate_scan_points_cylinder(radius, step, depth, azi, offset, laser_angle=90)
 
-        robot_control.move_robot_linear(robot, point)
-        sleep(2)
+    while best > 0.01:
+        robot_control.return_robot_to_start(robot)
+        robot_control.set_reference_coordinate_system(robot, [WOBJ_POS, q1])
 
-        transistor.laserON()
+        laser_data = []
 
-        laser_point = laser.measure()
-        if isinstance(laser_point, float):
-            laser_data.append([point[0][0], point[0][1], point[0][2], laser_point])
-        transistor.laserOff()
+        for point in points:
+            #alteredPoint = [point[0], [1, 0, 0, 0]]
 
-    laser_data = np.array(laser_data)
+            robot_control.move_robot_linear(robot, point)
+            sleep(2)
 
-    max_value = max(laser_data[:, -1])
-    min_value = min(laser_data[:, -1])
-    e = max_value - min_value
+            transistor.laserON()
 
-    i_max = np.where(laser_data[:, -1] == max_value)
-    i_min = np.where(laser_data[:, -1] == min_value)
+            laser_point = laser.measure()
+            if isinstance(laser_point, float):
+                laser_data.append([point[0][0], point[0][1], point[0][2], laser_point])
+            transistor.laserOff()
 
-    if len(i_max) > 1:
-        i_max = i_max[0]
+        laser_data = np.array(laser_data)
 
-    if len(i_min) > 1:
-        i_min = i_min[0]
+        max_value = max(laser_data[:, -1])
+        min_value = min(laser_data[:, -1])
+        e = max_value - min_value
 
-    data_max = laser_data[i_max]
-    data_min = laser_data[i_min]
+        i_max = np.where(laser_data[:, -1] == max_value)
+        i_min = np.where(laser_data[:, -1] == min_value)
 
-    dp = np.sqrt(np.sum((data_max[0][:2] - data_min[0][:2]) ** 2))
+        if len(i_max) > 1:
+            i_max = i_max[0]
 
-    angle = np.arctan(e / dp)
+        if len(i_min) > 1:
+            i_min = i_min[0]
 
-    v1 = data_max[0][:-1] - data_min[0][:-1]
-    v1 = v1 / np.linalg.norm(v1)
-    v2 = np.array([0, 0, 1])
-    normal = np.cross(v1, v2)
+        data_max = laser_data[i_max]
+        data_min = laser_data[i_min]
 
-    q = quaternion_from_axis_angle(np.append(normal, angle / 2))
-    q1 = concatenate_quaternions(q1=q1, q2=q)
+        dp = np.sqrt(np.sum((data_max[0][:2] - data_min[0][:2]) ** 2))
 
-    print(f"The difference between max and min: {e}")
+        angle = np.arctan(e / dp)
+
+        v1 = data_max[0][:-1] - data_min[0][:-1]
+        v1 = v1 / np.linalg.norm(v1)
+        v2 = np.array([0, 0, 1])
+        normal = np.cross(v1, v2)
+
+        q = quaternion_from_axis_angle(np.append(normal, angle / 2))
+        q1 = concatenate_quaternions(q1=q1, q2=q)
+
+        print(f"The difference between max and min: {e}")
+        print(f"With quaternions: {q1}")
+        if e < best:
+            best = e
+
+    print("")
+    print(f"The best difference between max and min: {best}")
     print(f"With quaternions: {q1}")
-    if e < best:
-        best = e
 
-print("")
-print(f"The best difference between max and min: {best}")
-print(f"With quaternions: {q1}")
+    return q1
